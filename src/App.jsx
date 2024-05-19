1d00bc4d9356335b8cdd3ad5bd7cd0b170740b84
akunjone <johnwitness0@gmail.com>
2024-05-19 18:02:37 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./navbar";
 import AppData from "./AppData";
 import Destinations from "./Destinations";
@@ -20,9 +20,8 @@ function App() {
     <BrowserRouter>
       <AutoScrollToTop>
         <div className="App">
-          <Router basename="uas-kelompok5-kelasb">
           <Navbar onLanguageChange={handleLanguageChange} language={language} />
-          <Switch>
+          <Routes>
             <Route
               path="/"
               element={<AppData language={language} onLanguageChange={handleLanguageChange} />}
@@ -59,8 +58,7 @@ function App() {
               path="/tentangkami"
               element={<Aboutus language={language} onLanguageChange={handleLanguageChange} />}
             />
-          </Switch>
-          </Router>
+          </Routes>
         </div>
         <footer className="footer">
           <Footer language={language} onLanguageChange={handleLanguageChange} />
