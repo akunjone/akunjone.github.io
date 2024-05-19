5b277bd9449ec8e3f5d25578bb5257721302dccf
Alif Nurfaiz <alifpriyatmoko@gmail.com>
2024-05-19 12:00:57 +0700

    Update Folklores.jsx
    Tambahan Folklore

--- src/Folklores.jsx
+++ src/Folklores.jsx
@@ -35,7 +35,53 @@ const folkloreData = [
     },
     image:
       "https://lh3.googleusercontent.com/d/1UmVleaN86ORHwuurOXhmpShYdtmVCdoS"
+  },
+  {
+    title: {
+      id: "Roro Mendut",
+      en: "Roro Mendut"
+    },
+    description: {
+      id: [
+        "Pada zaman dahulu, di pesisir pantai utara Pulau Jawa, tepatnya di daerah Pati, Jawa Tengah, tersebutlah sebuah desa nelayan bernama Teluk Cikal. Desa itu termasuk ke dalam wilayah Kadipaten Pati yang diperintah oleh Adipati Pragolo II. Kadipaten Pati sendiri merupakan salah satu wilayah taklukan dari Kesultanan Mataram yang dipimpin oleh Sultan Agung. Di Teluk Cikal, hidup seorang gadis anak nelayan bernama Rara Mendut. Ia seorang gadis yang cantik dan rupawan. Rara Mendut juga dikenal sebagai seorang gadis yang teguh pendirian. Ia tidak sungkan-sungkan menolak para lelaki yang datang melamarnya sebab ia sudah memiliki calon suami, yakni seorang pemuda desa yang tampan bernama Pranacitra, putra Nyai Singabarong yang terkenal kaya raya. Suatu hari, berita tentang kecantikan dan kemolekan Rara Mendut terdengar oleh Adipati Pragolo II. Penguasa Kadipaten Pati itu pun bermaksud menjadikannya sebagai selir. Sudah berkali-kali ia membujuknya, namun Rara Mendut tetap menolak. Merasa dikecewakan, Adipati Pragolo II mengutus beberapa pengawalnya untuk menculik Rara Mendut. Hari itu, ketika Rara Mendut sedang asyik menjemur ikan asin, datanglah utusan Adipati Progolo. 'Ayo gadis cantik, ikutlah kami keraton!' seru salah satu pengawal itu. 'Tak sudi aku menjadi selir tuanmu itu!'"
+      ],
+      en: [
+        "Once upon a time, on the northern coast of Java Island, specifically in Pati, Central Java, there was a fishing village named Teluk Cikal. The village was part of the Pati Duchy ruled by Duke Pragolo II. The Pati Duchy itself was one of the regions conquered by the Mataram Sultanate led by Sultan Agung. In Teluk Cikal, lived a girl named Rara Mendut, the daughter of a fisherman. She was a beautiful and charming girl. Rara Mendut was also known as a girl with a firm stance. She did not hesitate to reject the men who came to propose to her because she already had a fiancé, a handsome young man from the village named Pranacitra, the son of Nyai Singabarong who was a famous rich merchant. One day, news about the beauty and charm of Rara Mendut was heard by Duke Pragolo II. The ruler of the Pati Duchy intended to make her his concubine. He had tried to persuade her many times, but Rara Mendut kept refusing. Feeling disappointed, Duke Pragolo II sent several of his guards to kidnap Rara Mendut. That day, when Rara Mendut was busy drying salted fish, the envoy of Duke Progolo came. 'Come on beautiful girl, join us to the palace!' one of the guards shouted. 'I don't want to be your concubine!'"
+      ]
+    },
+    image: "https://lh3.googleusercontent.com/d/1gKB5Q0rFHA7vw2AS57lHQbHM4Mx9XGw3"
+  },
+  {
+    title: {
+      id: "Aji Saka",
+      en: "Aji Saka"
+    },
+    description: {
+      id: [
+        "Pada zaman dahulu, hiduplah seorang pemuda yang pandai, bijaksana, dan baik hati bernama Aji Saka. Ia tinggal di sebuah desa kecil di Jawa Tengah yang bernama Medang Kawit. Pria yang selalu mengenakan surban ini memiliki dua sahabat baik, yakni Dora dan Sembada. Ke mana pun Aji pergi, Dora dan Sembada selalu mengikuti. Mereka bertiga kerap pergi dari satu tempat ke tempat lain untuk menolong orang-orang yang mengalami kesulitan. Suatu hari, ketika mereka bertiga sedang berjalan-jalan di pegunungan Kendeng, ada suara pria yang terus-terusan teriak minta tolong. “Tolooong… Tolooonggg….” teriak pria itu. Mendengar jeritan tersebut, Aji dan kedua temannya pun langsung bergegas mencari sumber suara. Lalu, mereka mendapati seorang pria tua yang tengah dipukuli oleh preman keji. Seketika itu pula, mereka langsung memukul preman dan mencoba menyelamatkan pria tua tersebut. Setelah melalui pertikaian singkat, akhirnya pria itu dapat diselamatkan oleh Aji Saka. “Apa yang sedang kau lakukan di hutan, Kek? Kau tampak lemah dan tak berdaya,” ucap Dora. “Aku sedang melarikan diri, Nak. Desaku tak aman,” jawab pria tua itu. “Apa maksudmu, Kek? Dari mana asalmu? Siapa yang mengganggumu?” tanya Aji Saka. Lalu, kakek itu bercerita bila ia tinggal di Desa Medang Kamulan yang dipimpin oleh raja yang sangat keji, yakni Prabu Dewata Cengkar. Dulunya, ia sebenarnya adalah raja yang baik hati, tapi tiba-tiba ia berubah menjadi sangat kejam dan suka memakan daging manusia[^1^][4]."
+      ],
+      en: [
+        "There was a strong young man named Aji Saka who lived in the village of Medang Kawit. He had two helpers, Dora and Sembada, who were very loyal to him. Aji Saka wanted to go to a place called Medang Kamulan. But in Medang Kamulan, there was a bad king named Prabu Dewata Cengkar. He liked to hurt and eat people. The vice regent of Medang Kamulan, Patih Jugul Muda, had to find people for the king to eat. The people in Medang Kamulan were very scared and many of them ran away rather than be eaten by the king. Aji Saka wanted to stop the king from hurting and eating people. On the way to Medang Kamulan, Aji Saka and his helpers stopped at a place called Kendeng mountain. Aji Saka asked Sembada to stay there and gave him a special sword called a keris. Aji Saka said, “I’m giving you this special sword. Only give it back to me and no one else. I will come back for it.” Sembada agreed to do what Aji Saka said. Aji Saka and Dora continued their journey. Along the way, Aji Saka asked Dora to stay behind because he wanted to go to the Medang Kamulan kingdom alone. On his journey, Aji Saka met Patih Jugul Muda who looked worried because he couldn’t find any people to give to the evil king Dewata Cengkar. “If that’s what’s worrying you, give me to your king, Patih Jugul Muda,” Aji Saka said. Patih Jugul Muda was very surprised to hear Aji Saka’s words. While other people would be running and screaming if they were going to be sacrificed to the cruel king Dewata Cengkar, Aji Saka was offering himself willingly! Patih Jugul Muda then took Aji Saka to the royal palace of Medang Kamulan. Unlike other people who were very scared when they faced King Dewata Cengkar, Aji Saka was calm and not afraid at all."
+      ]
+    },
+    image: "https://lh3.googleusercontent.com/d/1uIlHqKNWGsJgVL7A0kJRgF62KtHy75dr"
+  },
+  {
+    title: {
+      id: "Jaka Tarub",
+      en: "Jaka Tarub"
+    },
+    description: {
+      id: [
+        "Pada masa lalu hiduplah seorang pemuda bernama Jaka Tarub. Dia merupakan pemuda desa yang gemar berburu. Suatu hari, dia sedang berburu burung di tengah hutan. Selama seharian Jaka Tarub belum menemukan burung buruan. Dia masuk hutan semakin dalam. Di tengah belantara, sayup-sayup dia mendengar suara beberapa wanita sedang berbincang. Agak mengendap-endap, Jaka Tarub berjalan ke arah suara itu. Ketika dia tiba di sana, dia melihat tujuh gadis sangat cantik sedang mandi. Dia benar-benar terpesona dengan kecantikan mereka. Setelah mereka selesai mandi, gadis-gadis itu perlahan mengambil selendang mereka. Hebatnya setelah mereka mengenakan selendang, mereka terbang ke langit. 'Mereka bukan manusia. Mereka Bidadari dari Kahyangan!' kata Jaka Tarub terkesima. Setelah itu Jaka Tarub pulang. Dia sangat gelisah. Dia terus memikirkan tujuh gadis cantik itu. Pada hari berikutnya, Jaka Tarub kemudian memutuskan untuk kembali ke tempat itu. Ketika para Bidadari itu mandi, dia mencuri salah satu selendangnya. Dan itu membuat satu peri tidak bisa terbang kembali ke langit. Dia menangis. Jaka Tarub kemudian mendekatinya. 'Ada apa? Kenapa kamu menangis?' 'Aku kehilangan selendangku. Aku tidak bisa kembali ke rumah. Semua saudariku telah meninggalkanku. Namaku Nawang Wulan. Aku akan memberimu apa pun jika kamu dapat menemukan syalku.' kata peri itu. 'Aku akan membantumu. Tetapi jika kita tidak dapat menemukannya, kamu bisa tinggal di rumahku. Kamu bisa menjadi istriku,' kata Jaka Tarub. Kemudian Jaka Tarub berpura-pura mencari selendang. Dan tentu saja mereka tidak dapat menemukannya. Setelah itu mereka pergi ke rumah Jaka Tarub. Kemudian mereka menikah. Mereka punya bayi perempuan. Mereka memiliki kehidupan yang bahagia. Mereka selalu punya cukup nasi untuk dimakan. Mereka tidak harus bekerja keras seperti tetangga mereka yang lain. Itu karena Nawang Wulan menggunakan sihirnya untuk memasak. Suatu hari, Jaka Tarub bertanya kepada istrinya mengenai keanehan beras mereka yang tidak pernah habis. Nawang Wulan tidak memberitahunya rahasia itu dan memintanya untuk jangan pernah membuka tutup panci saat memasak[^1^][1]."
+      ],
+      en: [
+        "Once upon a time, there lived a young man in the middle of a forest. The man's name is Jaka Tarub. Jaka Tarub really liked hunting animals. At that time, Jaka Tarub was exploring the forest to hunt. However, for a long time he didn’t find any animals. Finally, Jaka Tarub decided to rest and take a short nap. But he heard woman voice and he go to the place that he hear the voice. He was so shocked when he see 7 beautiful woman that take a bath in the lake.They are angels and jaka tarub want to get one from them. And finally he stole one of their shawl. And the shawl that he was stolen is nawang wulan shawl. Dewi nawang wulan is a youngest sister from all of the angel, when all of her sister can fly to the heaven, she can not because her shawl gone. When she was crying, jaka tarub come and bring her to his home. And not need long time, jaka tarub married with dewi nawang wulan and they had a daughter nawangsih. Dewi nawang wulan had a power and her power is gone when jaka tarub open rice steamer when she was in the river. When her power is gone, she doing all the human do like cooking, washing and all the activities. When he was opened rice barn and see her shawl. Nawang wulan so angry because her husband is liar. And because of him she can not go to the heaven. And finally she back to the heaven and leave jaka tarub and also her daughter nawangsih[^2^][7]."
+      ]
+    },
+    image: "https://lh3.googleusercontent.com/d/1EN_UnGVSfOuDc-bJPd6q3xhruDX_hOin"
   }
+     
 ];
 
 function Folk({ language }) {
